c9f63ea1eb0032072392d306cf3d9e9de887db78
javalikescript <javalikescript@free.fr>
2019-11-24 16:05:24 +0100

    Add ConBee plugin

--- lha/plugin/conbee/main.lua
+++ lha/plugin/conbee/main.lua
@@ -11,37 +11,31 @@ local Date = require('jls.util.Date')
 
 local CONST = {
   SENSORS = 'sensors',
-  GROUPS = 'groups',
-  LIGHTS = 'lights',
-  RULES = 'rules',
-  CONFIG = 'config',
-  RESOURCE_LINKS = 'resourcelinks',
-  CAPABILITIES = 'capabilities',
-  SCHEDULES = 'schedules'
+  CONFIG = 'config'
 }
 
-local Bridge = class.create(function(bridge)
+local Gateway = class.create(function(gateway)
 
-  function bridge:initialize(url, user)
+  function gateway:initialize(url, user)
     self.user = user or ''
     self.url = url or ''
     self.delta = 0
   end
 
-  function bridge:configure(config)
+  function gateway:configure(config)
     if config.UTC then
-      local bridgeTime = Date.fromISOString(config.UTC, true)
+      local gatewayTime = Date.fromISOString(config.UTC, true)
       --local time = Date.UTC()
       local time = Date.now()
-      self.delta = time - bridgeTime
-      logger:info('bridgeTime('..config.UTC..'): '..Date:new(bridgeTime):toISOString()..' time: '..Date:new(time):toISOString())
+      self.delta = time - gatewayTime
+      logger:info('gatewayTime('..config.UTC..'): '..Date:new(gatewayTime):toISOString()..' time: '..Date:new(time):toISOString())
     else
       self.delta = 0
     end
-    logger:info('using hue bridge delta time: '..tostring(self.delta))
+    logger:info('using ConBee gateway delta time: '..tostring(self.delta))
   end
 
-  function bridge:parseDateTime(dt)
+  function gateway:parseDateTime(dt)
     --logger:info('parseDateTime('..tostring(dt)..'['..type(dt)..'])')
     local d = Date.fromISOString(dt, true)
     if d then
@@ -49,14 +43,14 @@ local Bridge = class.create(function(bridge)
     end
   end
 
-  function bridge:getUrl(path)
+  function gateway:getUrl(path)
     if path then
       return self.url..self.user..'/'..path
     end
     return self.url..self.user..'/'
   end
 
-  function bridge:httpRequest(method, path, body)
+  function gateway:httpRequest(method, path, body)
     local client = http.Client:new({
       method = method,
       url = self.url..path,
@@ -72,7 +66,7 @@ local Bridge = class.create(function(bridge)
     --return http.request(self.url..self.user..'/'..path)
   end
 
-  function bridge:httpJson(method, path, t)
+  function gateway:httpJson(method, path, t)
     local b
     if t then
       b = json.encode(t)
@@ -81,45 +75,45 @@ local Bridge = class.create(function(bridge)
       local t = nil
       if body and #body > 0 then
         if logger:isLoggable(logger.DEBUG) then
-          logger:debug('bridge:httpJson('..method..') => #'..tostring(#body))
+          logger:debug('gateway:httpJson('..method..') => #'..tostring(#body))
         end
         t = json.decode(body)
         if logger:isLoggable(logger.FINE) then
-          logger:dump(t, 'bridge:httpJson('..method..')')
+          logger:dump(t, 'gateway:httpJson('..method..')')
         end
       end
       return t
     end)
   end
 
-  function bridge:httpUserJson(method, path, t)
+  function gateway:httpUserJson(method, path, t)
     return self:httpJson(method, self.user..'/'..path, t)
   end
 
-  function bridge:get(path)
+  function gateway:get(path)
     return self:httpUserJson('GET', path)
   end
 
-  function bridge:put(path, t)
+  function gateway:put(path, t)
     return self:httpUserJson('PUT', path, t)
   end
 
-  function bridge:post(path, t)
+  function gateway:post(path, t)
     return self:httpUserJson('POST', path, t)
   end
 
-  function bridge:updateConfiguration()
+  function gateway:updateConfiguration()
     return self:get(CONST.CONFIG):next(function(config)
       if config then
-        logger:info('update bridge configuration')
+        logger:info('update gateway configuration')
         self:configure(config)
       end
     end):catch(function(err)
-      logger:warn('fail to get bridge configuration, due to "'..tostring(err)..'"')
+      logger:warn('fail to get gateway configuration, due to "'..tostring(err)..'"')
     end)
   end
 
-  function bridge:createUser(applicationName, deviceName)
+  function gateway:createUser(applicationName, deviceName)
     return self:httpJson('POST', '', {
       devicetype = applicationName..'#'..deviceName
     })
@@ -152,20 +146,19 @@ tables.merge(configuration, {
   ids = {}
 }, true)
 
-local hueBridge = Bridge:new(configuration.url, configuration.user)
-logger:info('Hue bridge: "'..configuration.url..'"')
+local conbeeGateway = Gateway:new(configuration.url, configuration.user)
+logger:info('ConBee gateway: "'..configuration.url..'"')
 
 local deviceIdMap = configuration.ids
 
 local lastPollTime
 
 plugin:subscribeEvent('poll', function()
-  logger:info('poll hue plugin')
-  hueBridge:get(CONST.SENSORS):next(function(allSensors)
+  logger:info('poll ConBee plugin')
+  conbeeGateway:get(CONST.SENSORS):next(function(allSensors)
     local time = Date.now()
     if allSensors then
       for _, sensor in pairs(allSensors) do
-        -- see https://www.developers.meethue.com/documentation/supported-sensors
         if sensor.state and sensor.uniqueid then
           local id = deviceIdMap[sensor.uniqueid]
           if id and id ~= '' then
@@ -178,7 +171,7 @@ plugin:subscribeEvent('poll', function()
             local data = {}
             local lastupdatedTime
             if state.lastupdated and state.lastupdated ~= json.null then
-              lastupdatedTime = hueBridge:parseDateTime(state.lastupdated)
+              lastupdatedTime = conbeeGateway:parseDateTime(state.lastupdated)
               --logger:info('device '..id..'('..sensor.type..') last updated: '..Date:new(lastupdatedTime):toISOString())
             end
             if sensor.type == 'ZLLLightLevel' and state.lightlevel ~= json.null then
@@ -186,16 +179,22 @@ plugin:subscribeEvent('poll', function()
               -- Logarithm scale used because the human eye adjusts to light levels and small changes at low lux levels are more noticeable than at high lux levels.  
               data.lightlevel = state.lightlevel
               -- dark, daylight true/false, lastupdated
-            elseif sensor.type == 'ZLLTemperature' and state.temperature ~= json.null then
+            elseif (sensor.type == 'ZLLTemperature') or (sensor.type == 'ZHATemperature') and state.temperature ~= json.null then
               -- Current temperature in 0.01 degrees Celsius. (3000 is 30.00 degree)
               -- lastupdated
               data.temperature = state.temperature / 100
+            elseif sensor.type == 'ZHAHumidity' and state.humidity ~= json.null then
+              -- lastupdated
+              data.humidity = state.humidity / 100
+            elseif sensor.type == 'ZHAPressure' and state.pressure ~= json.null then
+              -- lastupdated
+              data.pressure = state.pressure
             elseif sensor.type == 'ZLLPresence' and state.presence ~= json.null then
               -- presence True if sensor detects presence
               -- lastupdated Last time the sensor state was updated, probably UTC
               -- see https://developers.meethue.com/develop/hue-api/supported-devices/
               if lastupdatedTime and lastPollTime then
-                data.presence = lastupdatedTime >= lastPollTime and lastupdatedTime < time
+                data.presence = state.presence and lastupdatedTime >= lastPollTime and lastupdatedTime < time
               else
                 data.presence = state.presence
               end
@@ -216,59 +215,17 @@ plugin:subscribeEvent('poll', function()
     -- cleaning data in case of polling failure
     plugin:setDevicesData({})
   end)
-
-  -- hueBridge:put(CONST.GROUPS..'/'..groupId..'/action', {on = value})
-
-  hueBridge:get(CONST.LIGHTS):next(function(allLights)
-    if allLights then
-      for lightId, light in pairs(allLights) do
-        local id = deviceIdMap[light.uniqueid]
-        if id and id ~= '' and light.state then
-          local device = plugin:getDevice(id)
-          if not device then
-            logger:info('register device '..id)
-            device = plugin:registerDevice(id, {})
-            logger:info('watching on '..device:getPath('state/on'))
-            device:watchDataValue(device:getPath('state/on'), function(value, previousValue, path)
-              logger:info('hue light '..id..' change')
-              hueBridge:put(CONST.LIGHTS..'/'..lightId..'/state', {on = value})
-            end)
-          end
-          device:applyDeviceData({
-            state = {
-              on = light.state.on
-            }
-          })
-        elseif not id then
-          deviceIdMap[sensor.uniqueid] = ''
-          logger:info('new device found '..sensor.uniqueid)
-        end
-      end
-    else
-      -- cleaning data in case of polling failure
-      plugin:setDevicesData({})
-    end
-  end):catch(function(err)
-    logger:warn('fail to get lights, due to "'..tostring(err)..'"')
-    -- cleaning data in case of polling failure
-    plugin:setDevicesData({})
-  end)
 end)
 
---httpServer:createContext('/bridge/(.*)', httpHandler.redirect, {url = hueBridge:getUrl()})
-
 plugin:subscribeEvent('refresh', function()
-  logger:info('refresh hue plugin')
-  hueBridge:updateConfiguration()
+  logger:info('refresh ConBee plugin')
+  conbeeGateway:updateConfiguration()
   -- refresh deviceIdMap
 end)
 
 plugin:subscribeEvent('startup', function()
-  logger:info('startup hue plugin')
-  hueBridge:updateConfiguration()
+  logger:info('startup ConBee plugin')
+  conbeeGateway:updateConfiguration()
   -- refresh deviceIdMap
-  plugin:onPlugin('web_base', function(webSamplePlugin)
-    webSamplePlugin:registerAddonPlugin(plugin)
-  end)
 end)
 
