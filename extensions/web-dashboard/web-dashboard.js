885953398af7223d6bd58532072160862432fc2b
javalikescript <javalikescript@free.fr>
2022-04-02 08:53:26 +0200

    Clean dashboard

--- extensions/web-dashboard/web-dashboard.js
+++ extensions/web-dashboard/web-dashboard.js
@@ -1,34 +1,15 @@
 define(['./web-dashboard.xml'], function(dashboardTemplate) {
 
-  /*
-  We want to present an overview of things on a dashboard.
-  The dashboard contains tiles representing one or more aggregated things.
-  A tile has a title, an aggegation type, an optional group.
-  An aggregation type computes a value, an icon
-  The wanted tiles are:
-    air condition: temperature, humidity, pressure, smoke
-    weather: temperature, humidity, pressure, rain, wind, cloud
-    light: on/off, intensity, color
-    motion: presence
-
-  */
-  var typeByName = {
-    temperature: 'temperature',
-    presence: 'presence',
-    on: 'light'
+  var typeByCapability = {
+    "Light": "OnOffProperty",
+    "TemperatureSensor": "TemperatureProperty",
+    "MotionSensor": "MotionProperty",
+    "HumiditySensor": "HumidityProperty",
+    "BarometricPressureSensor": "BarometricPressureProperty",
+    "SmokeSensor": "SmokeProperty"
   };
-  var nameByType = swapMap(typeByName);
 
-  function guessAggregation(thing, properties) {
-    if (thing && properties) {
-      for (var name in typeByName) {
-        if (name in properties) {
-          return typeByName[name];
-        }
-      }
-    }
-    return 'unknown';
-  }
+  //var capabilityToType = swapMap(typeByCapability);
 
   function lightLevelToLux(value) {
     return Math.round((Math.pow(10, value / 10000) - 1) * 100) / 100;
@@ -61,65 +42,55 @@ define(['./web-dashboard.xml'], function(dashboardTemplate) {
         }));
       },
       processThings: function(config, things, properties) {
+        //console.log('processThings()', config, things, properties);
         var tiles = [];
-        var tilesDef = config.tiles;
-        if (tilesDef) {
-          for (var i = 0; i < tilesDef.length; i++) {
-            var tileDef = tilesDef[i];
-            //console.log('tile ' + i, JSON.stringify(tileDef, undefined, 2));
-            var thingIds = tileDef.thingIds;
-            var tile = Object.assign({}, tileDef);
-            var list = [];
-            var anyProps = {}
-            for (var j = 0; j < thingIds.length; j++) {
-              var thingId = thingIds[j];
-              var thing = things[thingId];
-              var props = properties[thingId];
-              list.push({
-                thing: thing,
-                props: props
-              });
-              anyProps = Object.assign(anyProps, props);
+        var thingsDef = config.things;
+        if (thingsDef) {
+          for (var i = 0; i < thingsDef.length; i++) {
+            var thingDef = thingsDef[i];
+            var thingId = thingDef.thingId;
+            var thing = things[thingId];
+            if (!thing) {
+              console.log('Thing not found "' + thingId + '"', thingDef);
+              continue;
             }
-            if (tile.type === 'auto') {
-              tile.type = guessAggregation(list[0].thing, anyProps);
+            var tile = Object.assign({
+              title: thing.title,
+              value: ''
+            }, thingDef);
+            var type = thingDef.type;
+            if ((type === 'auto') || !type) {
+              var thingType = thing['@type'][0];
+              type = typeByCapability[thingType];
             }
-            var names = Object.keys(anyProps);
-            var aggByName = {};
-            var valuesByName = {};
-            for (var k = 0; k < names.length; k++) {
-              var name = names[k];
-              var values = [];
-              for (var l = 0; l < list.length; l++) {
-                var value = list[l].props[name];
-                if (value !== undefined) {
-                  values.push(value);
-                }
+            if (!type) {
+              console.log('Thing type not found', tile);
+              continue;
+            }
+            var propertyName;
+            var unit;
+            for (var propName in thing.properties) {
+              var prop = thing.properties[propName];
+              var propType = prop['@type'];
+              propertyName = propName;
+              unit = prop.unit;
+              if (propType === type) {
+                break;
               }
-              valuesByName[name] = values;
-              var agg;
-              switch(typeof values[0]) {
-                case 'boolean':
-                  agg = values.indexOf(true) >= 0;
-                  break;
-                case 'number':
-                  var sum = values.reduce(function(pv, cv) { return pv + cv; }, 0);
-                  agg = sum / values.length;
-                  break;
-              }
-              aggByName[name] = agg;
             }
-            tile.aggByName = aggByName;
-            var name = nameByType[tile.type];
-            if (name in aggByName) {
-              tile.value = aggByName[name];
-            } else {
-              tile.value = '?';
+            if (!propertyName) {
+              console.log('Thing property not found', tile);
+              continue;
+            }
+            var props = properties[thingId];
+            if (props) {
+              tile.value = props[propertyName];
+              tile.unit = unit;
             }
             tiles.push(tile);
           }
         }
-        //console.log('tiles', JSON.stringify(tiles, undefined, 2));
+        console.log('tiles', JSON.stringify(tiles, undefined, 2));
         this.tiles = tiles;
       },
       onAdd: function() {
