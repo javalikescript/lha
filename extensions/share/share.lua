62fac7494b86023ac36d628647960ce945ef34d1
javalikescript <javalikescript@free.fr>
2022-04-15 22:49:32 +0200

    Add reverse proxy to share

--- extensions/share/share.lua
+++ extensions/share/share.lua
@@ -2,6 +2,8 @@ local extension = ...
 
 local logger = require('jls.lang.logger')
 local FileHttpHandler = require('jls.net.http.handler.FileHttpHandler')
+local ProxyHttpHandler = require('jls.net.http.handler.ProxyHttpHandler')
+local WebDavHttpHandler = require('jls.net.http.handler.WebDavHttpHandler')
 local utils = require('lha.utils')
 
 local contexts = {}
@@ -18,6 +20,14 @@ local function addContext(server, ...)
   table.insert(contexts, context)
 end
 
+local function isValidPath(name)
+  if name == 'engine' or name == 'things' then
+    logger:warn('Invalid share name "'..name..'"')
+    return false
+  end
+  return true
+end
+
 extension:subscribeEvent('startup', function()
   local configuration = extension:getConfiguration()
   local server = extension:getEngine():getHTTPServer()
@@ -25,15 +35,28 @@ extension:subscribeEvent('startup', function()
   if configuration.shares then
     for _, share in ipairs(configuration.shares) do
       local dir = utils.getAbsoluteFile(share.dir or 'share', extension:getDir())
-      if dir:isDirectory() then
-        logger:info('Using share directory "'..dir:getPath()..'"')
-      else
+      if not dir:exists() then
+        logger:warn('Share directory "'..dir:getPath()..'" not found')
+      elseif not dir:isDirectory() then
         logger:warn('Invalid share directory "'..dir:getPath()..'"')
       end
-      if share.name == 'engine' or share.name == 'things' then
-        logger:warn('Invalid share name "'..share.name..'"')
-      else
-        addContext(server, '/'..share.name..'/(.*)', FileHttpHandler:new(dir, share.permissions))
+      if isValidPath(share.name) then
+        logger:info('Share directory "'..dir:getPath()..'" on "'..tostring(share.name)..'"')
+        local path = '/'..share.name..'/(.*)'
+        if share.useWebDAV then
+          addContext(server, path, WebDavHttpHandler:new(dir, share.permissions))
+        else
+          addContext(server, path, FileHttpHandler:new(dir, share.permissions))
+        end
+      end
+    end
+  end
+  if configuration.proxies then
+    for _, proxy in ipairs(configuration.proxies) do
+      if isValidPath(proxy.name) and proxy.url then
+        logger:info('Reverse proxy to "'..tostring(proxy.url)..'" on "'..tostring(proxy.name)..'"')
+        local path = '/'..proxy.name..'/(.*)'
+        addContext(server, path, ProxyHttpHandler:new():configureReverse(proxy.url))
       end
     end
   end
