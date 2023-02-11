9cd36c57ba448cc7950ea7ca9267166816276e55
javalikescript <javalikescript@free.fr>
2023-02-11 18:40:36 +0100

    Upgrade historical table handler

--- extensions/web-chart/web-chart.lua
+++ extensions/web-chart/web-chart.lua
@@ -1,5 +1,6 @@
 local extension = ...
 
+local class = require('jls.lang.class')
 local logger = require('jls.lang.logger')
 local RestHttpHandler = require('jls.net.http.handler.RestHttpHandler')
 local HttpExchange = require('jls.net.http.HttpExchange')
@@ -7,63 +8,69 @@ local strings = require('jls.util.strings')
 local tables = require('jls.util.tables')
 local Date = require('jls.util.Date')
 
-local function historicalDataHandler(exchange)
-  if HttpExchange.methodAllowed(exchange, 'GET') then
-    local context = exchange:getContext()
-    local dataHistory = context:getAttribute('dataHistory')
-    local path = exchange:getRequestArguments()
-    local request = exchange:getRequest()
-    local toTime = tonumber(request:getHeader("X-TO-TIME"))
-    if toTime then
-      toTime = toTime * 1000
-    end
-    local period = tonumber(request:getHeader("X-PERIOD"))
-    if not period then
-      local t
-      local tp = string.gsub(path, '/$', '')
+local HistoricalDataHandler = class.create('jls.net.http.HttpHandler', function(tableHandler)
+
+  function tableHandler:initialize(dataHistory)
+    self.dataHistory = dataHistory
+  end
+
+  function tableHandler:handle(exchange)
+    if HttpExchange.methodAllowed(exchange, 'GET') then
+      local path = exchange:getRequestArguments()
+      local request = exchange:getRequest()
+      local toTime = tonumber(request:getHeader("X-TO-TIME"))
       if toTime then
-        t = dataHistory:getTableAt(toTime) or {}
-      else
-        t = dataHistory:getLiveTable()
+        toTime = toTime * 1000
       end
-      RestHttpHandler.replyJson(exchange, {
-        value = tables.getPath(t, '/'..tp)
-      })
-      return
-    end
-    local fromTime = tonumber(request:getHeader("X-FROM-TIME"))
-    local subPaths = request:getHeader("X-PATHS")
-    if logger:isLoggable(logger.FINE) then
-      logger:fine('process data request '..tostring(fromTime)..' - '..tostring(toTime)..' / '..tostring(period)..' on "'..tostring(path)..'"')
-    end
-    period = period * 1000
-    if not toTime then
-      toTime = Date.now()
-    end
-    if fromTime then
-      fromTime = fromTime * 1000
-    else
-      -- use 100 data points by default
-      fromTime = toTime - period * 100
-    end
-    if fromTime < toTime and ((toTime - fromTime) / period) < 10000 then
-      local result
-      if subPaths then
-        local paths = strings.split(subPaths, ',')
-        for i = 1, #paths do
-          paths[i] = path..paths[i]
+      local period = tonumber(request:getHeader("X-PERIOD"))
+      if not period then
+        local t
+        local tp = string.gsub(path, '/$', '')
+        if toTime then
+          t = self.dataHistory:getTableAt(toTime) or {}
+        else
+          t = self.dataHistory:getLiveTable()
         end
-        result = dataHistory:loadMultiValues(fromTime, toTime, period, paths)
-      else
-        -- TODO provide property types
-        result = dataHistory:loadValues(fromTime, toTime, period, path)
+        RestHttpHandler.replyJson(exchange, {
+          value = tables.getPath(t, '/'..tp)
+        })
+        return
       end
-      RestHttpHandler.replyJson(exchange, result)
-    else
-      HttpExchange.badRequest(exchange)
+      local fromTime = tonumber(request:getHeader("X-FROM-TIME"))
+      local subPaths = request:getHeader("X-PATHS")
+      if logger:isLoggable(logger.FINE) then
+        logger:fine('process data request '..tostring(fromTime)..' - '..tostring(toTime)..' / '..tostring(period)..' on "'..tostring(path)..'"')
+      end
+      period = period * 1000
+      if not toTime then
+        toTime = Date.now()
+      end
+      if fromTime then
+        fromTime = fromTime * 1000
+      else
+        -- use 100 data points by default
+        fromTime = toTime - period * 100
+      end
+      if fromTime < toTime and ((toTime - fromTime) / period) < 10000 then
+        local result
+        if subPaths then
+          local paths = strings.split(subPaths, ',')
+          for i = 1, #paths do
+            paths[i] = path..paths[i]
+          end
+          result = self.dataHistory:loadMultiValues(fromTime, toTime, period, paths)
+        else
+          -- TODO provide property types
+          result = self.dataHistory:loadValues(fromTime, toTime, period, path)
+        end
+        RestHttpHandler.replyJson(exchange, result)
+      else
+        HttpExchange.badRequest(exchange)
+      end
     end
   end
-end
+
+end)
 
 local context
 
@@ -78,9 +85,7 @@ extension:subscribeEvent('startup', function()
   local server = engine:getHTTPServer()
   cleanup(server)
   -- TODO Move to extension handler
-  context = server:createContext('/engine/historicalData/(.*)', historicalDataHandler, {
-    dataHistory = engine.dataHistory
-  })
+  context = server:createContext('/engine/historicalData/(.*)', HistoricalDataHandler:new(engine.dataHistory))
   engine:onExtension('web-base', function(webBaseExtension)
     webBaseExtension:registerAddonExtension(extension, true)
   end)
